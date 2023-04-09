5a491d6e0edc64e5cac6b16c8d3f0b43164e2a3a
Rahul Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-04-09 15:28:09 +0000

    added more mock data

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,18 +4,27 @@ import maleProfile from "./images/maleProfile.jpg";
 
 const Employees = () => {
   const [employees, setEmployees] = useState([
-    { id: 1, fullName: "Bob Jones", designation: "JavaScript Developer", gender: "male", teamName: "TeamA" },
-    { id: 2, fullName: "Jill Bailey", designation: "Node Developer", gender: "female", teamName: "TeamA" },
-    { id: 3, fullName: "Gail Shepherd", designation: "Java Developer", gender: "female", teamName: "TeamA" },
-    { id: 4, fullName: "Sam Reynolds", designation: "React Developer", gender: "male", teamName: "TeamB" },
-    { id: 5, fullName: "David Henry", designation: "DotNet Developer", gender: "male", teamName: "TeamB" },
-    { id: 6, fullName: "Sarah Blake", designation: "SQL Server DBA", gender: "female", teamName: "TeamB" },
-    { id: 7, fullName: "James Bennet", designation: "Angular Developer", gender: "male", teamName: "TeamC" },
-    { id: 8, fullName: "Jessica Faye", designation: "API Developer", gender: "female", teamName: "TeamC" },
-    { id: 9, fullName: "Lita Stone", designation: "C++ Developer", gender: "female", teamName: "TeamC" },
-    { id: 10, fullName: "Daniel Young", designation: "Python Developer", gender: "male", teamName: "TeamD" },
-    { id: 11, fullName: "Adrian Jacobs", designation: "Vue Developer", gender: "male", teamName: "TeamD" },
-    { id: 12, fullName: "Devin Monroe", designation: "Graphic Designer", gender: "male", teamName: "TeamD" }
+    { "id": 1, "fullName": "Merridie Labrenz", "designation": "React Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 2, "fullName": "Tildi Gunstone", "designation": "Node Developer", "gender": "male", "teamName": "Team-A" },
+    { "id": 3, "fullName": "Sergei Corriea", "designation": "Javascript Developer", "gender": "female", "teamName": "Team-A" },
+    { "id": 4, "fullName": "Callida Illston", "designation": "Javascript Developer", "gender": "male", "teamName": "Team-A" },
+    { "id": 5, "fullName": "Sollie Fomichyov", "designation": "Javascript Developer", "gender": "male", "teamName": "Team-B" },
+    { "id": 6, "fullName": "Niki Blamire", "designation": "Java Developer", "gender": "male", "teamName": "Team-B" },
+    { "id": 7, "fullName": "Bertrando Elcome", "designation": "Python Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 8, "fullName": "Cyndi Manclark", "designation": "Node Developer", "gender": "male", "teamName": "Team-B" },
+    { "id": 9, "fullName": "Matty Brammall", "designation": "SQL Developer", "gender": "female", "teamName": "Team-A" },
+    { "id": 10, "fullName": "Tabbie Yaakov", "designation": "Java Developer", "gender": "female", "teamName": "Team-A" },
+    { "id": 11, "fullName": "Cori Hubbins", "designation": "Python Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 12, "fullName": "Con Melville", "designation": "Java Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 13, "fullName": "Dalli Shimmings", "designation": "Javascript Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 14, "fullName": "Patrizia Sarjant", "designation": "Java Developer", "gender": "male", "teamName": "Team-B" },
+    { "id": 15, "fullName": "Lauraine Jackalin", "designation": "Python Developer", "gender": "male", "teamName": "Team-B" },
+    { "id": 16, "fullName": "Ebenezer Josskovitz", "designation": "Python Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 17, "fullName": "Aggi Graves", "designation": "Javascript Developer", "gender": "female", "teamName": "Team-A" },
+    { "id": 18, "fullName": "Horton Falkus", "designation": "Node Developer", "gender": "female", "teamName": "Team-B" },
+    { "id": 19, "fullName": "Lainey Collar", "designation": "Java Developer", "gender": "female", "teamName": "Team-A" },
+    { "id": 20, "fullName": "Filip Brockley", "designation": "Java Developer", "gender": "male", "teamName": "Team-A" },
+    { "id": 21, "fullName": "Yetta Fidock", "designation": "React Developer", "gender": "female", "teamName": "Team-A" }
   ]);
 
   return (
@@ -27,9 +36,9 @@ const Employees = () => {
               employees.map((emp) => (
                 <div id={emp.id} className="card m-2" style={{ cursor: "pointer" }}>
 
-             {/*  <img src={emp.gender === "male" ? maleProfile : femaleProfile} className="card-img-top" /> */}
-                  {(emp.gender === "male") ? <img src={maleProfile} className="card-img-top" /> 
-                                           : <img src={femaleProfile} className="card-img-top" />}
+                  {/*  <img src={emp.gender === "male" ? maleProfile : femaleProfile} className="card-img-top" /> */}
+                  {(emp.gender === "male") ? <img src={maleProfile} className="card-img-top" />
+                    : <img src={femaleProfile} className="card-img-top" />}
                   <div className="card-body">
                     <h5 className="card-title">Full Name: {emp.fullName}</h5>
                     <p className="card-text"><b>Designation: </b>{emp.designation}</p>
