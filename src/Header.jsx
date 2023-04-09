0787055e018fc861a1476399f423d4a47064659e
Rahul Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-04-09 22:17:46 +0000

    small bug fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
-const Header = ({selectedTeam, teamMemberCount}) => {
+const Header = ({ selectedTeam, teamMemberCount }) => {
   return (
     <header className="text-center">
       <h1 className="mt-4 p5 text-center bg-primary text-white rounded" >Team Member Allocation</h1>
-      <h3> {selectedTeam} has {teamMemberCount} members</h3>
+      <h3> {selectedTeam} has {teamMemberCount} {teamMemberCount < 2 ? "member" : "members"}</h3>
     </header>
   );
 }
