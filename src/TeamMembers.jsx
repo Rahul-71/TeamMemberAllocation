cfeac972df3d2407ced1e91a37436a09a0916894
Rahul Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-04-10 05:11:50 +0000

    seperated team members code from employees.jsx to TeamMembers.jsx

---
+++ src/TeamMembers.jsx
@@ -0,0 +1,29 @@
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
+
+const TeamMembers = ({ employees, selectedTeam, employeeCardClickHandler }) => {
+  return (
+    employees.map((emp) => (
+      <div
+        key={emp.id}
+        id={emp.id}
+        className={(emp.teamName === selectedTeam) ? "card m-2 standout" : "card m-2"}
+        style={{ cursor: "pointer" }}
+        onClick={employeeCardClickHandler} >
+
+        {/*  <img src={emp.gender === "male" ? maleProfile : femaleProfile} className="card-img-top" /> */}
+        {/* conditional rendering */}
+        {(emp.gender === "male")
+          ? <img src={maleProfile} className="card-img-top" />
+          : <img src={femaleProfile} className="card-img-top" />}
+
+        <div className="card-body">
+          <h5 className="card-title">Full Name: {emp.fullName}</h5>
+          <p className="card-text"><b>Designation: </b>{emp.designation}</p>
+        </div>
+      </div>
+    ))
+  );
+}
+
+export default TeamMembers;
